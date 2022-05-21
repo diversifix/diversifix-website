72292173400bef9fdc1e9d2afba75e40057d9725
David Pomerenke <pomerenk@pik-potsdam.de>
2022-05-21 10:52:08 +0200

    Use https git path

--- build-app.sh
+++ build-app.sh
@@ -1,7 +1,9 @@
 rm -rf diversifix-app
 rm -rf public/diversifix-app
 rm -rf build
-git clone git@github.com:diversifix/diversifix.git diversifix-app --depth 1
-echo "SKIP_PREFLIGHT_CHECK=true" > diversifix-app/react-ui/.env
-cd diversifix-app/react-ui && SKIP_PREFLIGHT_CHECK=true yarn install && yarn build && cp -R build ../../public/diversifix-app
+git clone https://github.com/diversifix/diversifix.git diversifix-app --depth 1 && \
+echo "SKIP_PREFLIGHT_CHECK=true" > diversifix-app/react-ui/.env && \
+cd diversifix-app/react-ui && SKIP_PREFLIGHT_CHECK=true yarn install && \
+yarn build && \
+cp -R build ../../public/diversifix-app && \
 cd ../..
