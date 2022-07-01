9ee91b9803bbcde532599ca8f1db48850a533946
David Pomerenke <pomerenk@pik-potsdam.de>
2022-07-01 21:46:43 +0200

    Add addin support site

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Verein } from "./Pages/Verein";
 import { Mitmachen } from "./Pages/Mitmachen";
 import { Main } from "./Pages/Main";
 import { Impressum } from "./Pages/Impressum";
+import { Support } from "./Pages/Support";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="verein" element={<Verein />}></Route>
           <Route path="mitmachen" element={<Mitmachen />}></Route>
           <Route path="impressum" element={<Impressum />}></Route>
+          <Route path="support" element={<Support />}></Route>
         </Route>
       </Routes>
     </BrowserRouter>
