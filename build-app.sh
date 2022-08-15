7560d04fa4c697e484f27e3be2344aace7acdb50
David Pomerenke <pomerenk@pik-potsdam.de>
2022-08-15 12:57:06 +0200

    Fix VCS revision publishing

--- build-app.sh
+++ build-app.sh
@@ -5,5 +5,7 @@ git clone https://github.com/diversifix/diversifix.git diversifix-app --depth 1 
 echo "SKIP_PREFLIGHT_CHECK=true" > diversifix-app/react-ui/.env && \
 cd diversifix-app/react-ui && \ 
 yarn install && \
-REACT_APP_API_BASE_URL="https://api.diversifix.org/v2" yarn build && \
+REACT_APP_API_BASE_URL="https://api.diversifix.org/v2" \
+REACT_APP_VCS_REVISION=$(git rev-parse HEAD) \
+yarn build && \
 cp -R build ../../public/diversifix-app
