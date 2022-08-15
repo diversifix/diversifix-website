166166cc445545433608538e3a75dde766a35385
David Pomerenke <pomerenk@pik-potsdam.de>
2022-08-15 15:09:06 +0200

    Fix typo & upgrade dependencies

--- build-app.sh
+++ build-app.sh
@@ -3,7 +3,7 @@ rm -rf public/diversifix-app
 rm -rf build
 git clone https://github.com/diversifix/diversifix.git diversifix-app --depth 1 && \
 echo "SKIP_PREFLIGHT_CHECK=true" > diversifix-app/react-ui/.env && \
-cd diversifix-app/react-ui && \ 
+cd diversifix-app/react-ui && \
 yarn install && \
 REACT_APP_API_BASE_URL="https://api.diversifix.org/v2" \
 REACT_APP_VCS_REVISION=$(git rev-parse HEAD) \
