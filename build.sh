75880bbca7d86a102294d1143fbe9812994871ec
David Pomerenke <davidpomerenke@mailbox.org>
2022-10-08 23:05:45 +0200

    Fix bug with path

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ rm -rf diversifix
 rm -rf app
 git clone https://github.com/diversifix/diversifix.git --depth 1 && \
 echo "SKIP_PREFLIGHT_CHECK=true" > diversifix-app/react-ui/.env && \
-cd diversifix-app/react-ui && \
+cd diversifix/react-ui && \
 yarn install && \
 REACT_APP_API_BASE_URL="https://api.diversifix.org/v2" \
 REACT_APP_VCS_REVISION=$(git rev-parse HEAD) \
